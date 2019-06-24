bcab16dbe42de364a852088a5f071542fc97a8fe
djl <yuuyoo@163.com>
2019-06-24 21:49:11 +0800

    Eureka注册中心完成

---
+++ ikeeping_eureka/src/main/java/com/zz/ikeeping/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zz.ikeeping.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author DJl
+ * @create 2019-06-24 21:07
+ * @desc Eureka注册中心启动开关
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
