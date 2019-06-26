7521f52377fdbc76f0b953f5a85fade8b43b7870
djl <yuuyoo@163.com>
2019-06-26 21:43:17 +0800

    七牛云平台配置参数添加

---
+++ ikeeping_common/src/main/java/com/zz/ikeeping/common/config/KodoConfig.java
@@ -0,0 +1,14 @@
+package com.zz.ikeeping.common.config;
+
+/**
+ * @author DJL
+ * @create 2019-06-26 21:13
+ * @desc 七牛云存储配置信息
+ **/
+public class KodoConfig {
+
+    // 七牛KODO
+    public static final String KODO_ACCESSKEY = "M99LJ7JghYv9eEYhSqu0CVYEZvft24Wj838PBexk";
+    public static final String KODO_SECRETKEY = "1Ncq8DFnSxT-kPckxC9s4Tz9_QJ09K7EZeQpuyMG";
+    public static final String KODO_BUCKET = "djlshop";
+}
