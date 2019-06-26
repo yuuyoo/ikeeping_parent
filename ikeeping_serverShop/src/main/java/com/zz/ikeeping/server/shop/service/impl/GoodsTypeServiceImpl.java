f92a53f4b817139b712ebfdd2a7acf26bb66e270
renr <tfy>
2019-06-26 21:29:43 +0800

    1. 修正： serverShop子项目，pom文件 2. 新增： 查询所有商品分类

---
+++ ikeeping_serverShop/src/main/java/com/zz/ikeeping/server/shop/service/impl/GoodsTypeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.zz.ikeeping.server.shop.service.impl;
+
+import com.zz.ikeeping.common.vo.R;
+import com.zz.ikeeping.server.shop.dao.GoodsTypeMapper;
+import com.zz.ikeeping.server.shop.service.GoodsTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoodsTypeServiceImpl implements GoodsTypeService {
+
+    @Autowired
+    private GoodsTypeMapper goodsTypeDao;
+
+    @Override
+    public R selectAll() {
+        return R.setOK(goodsTypeDao.selectAll());
+    }
+}
