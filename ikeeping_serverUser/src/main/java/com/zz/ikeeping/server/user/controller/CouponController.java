16e6408088c9e60b28e1d9224f6bbced30a6daa2
hmy <156897@aliyun.com>
2019-06-28 20:02:53 +0800

    修改

--- ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/controller/CouponController.java
+++ ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/controller/CouponController.java
@@ -1,15 +1,15 @@
-package com.zz.ikeeping.userserver.coupon.controller;
+package com.zz.ikeeping.server.user.controller;
 
 import com.zz.ikeeping.common.vo.R;
 import com.zz.ikeeping.entity.Coupon;
-import com.zz.ikeeping.userserver.coupon.service.UserService;
+import com.zz.ikeeping.server.user.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class UserController {
+public class CouponController {
     @Autowired
-    private UserService userService;
+    private CouponService userService;
     @PostMapping("user/coupon/save.do")
     public R save(@RequestBody Coupon coupon){
         return userService.save(coupon);
--- ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/service/impl/CouponServiceimpl.java
+++ ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/service/impl/CouponServiceimpl.java
@@ -1,13 +1,13 @@
-package com.zz.ikeeping.userserver.coupon.service.impl;
+package com.zz.ikeeping.server.user.service.impl;
 import com.zz.ikeeping.common.vo.R;
 import com.zz.ikeeping.entity.Coupon;
-import com.zz.ikeeping.userserver.coupon.dao.CouponMapper;
-import com.zz.ikeeping.userserver.coupon.service.UserService;
+import com.zz.ikeeping.server.user.dao.CouponMapper;
+import com.zz.ikeeping.server.user.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceimpl implements UserService {
+public class CouponServiceimpl implements CouponService {
     @Autowired
     private CouponMapper couponMapper;
 
