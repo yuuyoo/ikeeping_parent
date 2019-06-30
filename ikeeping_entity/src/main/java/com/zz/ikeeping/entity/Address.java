5525e2182f18d829c1219c3c1ad6043885b0a9ba
qi <541694643@qq.com>
2019-06-30 12:27:47 +0800

    基于短信验证码实现注册功能

--- ikeeping_entity/src/main/java/com/zz/ikeeping/entity/Address.java
+++ ikeeping_entity/src/main/java/com/zz/ikeeping/entity/Address.java
@@ -1,5 +1,8 @@
 package com.zz.ikeeping.entity;
 
+import lombok.Data;
+
+@Data
 public class Address {
     private Integer addid;
 
@@ -13,51 +16,5 @@ public class Address {
 
     private Integer flag;
 
-    public Integer getAddid() {
-        return addid;
-    }
 
-    public void setAddid(Integer addid) {
-        this.addid = addid;
-    }
-
-    public Integer getUid() {
-        return uid;
-    }
-
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
-
-    public String getAddname() {
-        return addname;
-    }
-
-    public void setAddname(String addname) {
-        this.addname = addname == null ? null : addname.trim();
-    }
-
-    public String getAddtel() {
-        return addtel;
-    }
-
-    public void setAddtel(String addtel) {
-        this.addtel = addtel == null ? null : addtel.trim();
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
-
-    public Integer getFlag() {
-        return flag;
-    }
-
-    public void setFlag(Integer flag) {
-        this.flag = flag;
-    }
 }
