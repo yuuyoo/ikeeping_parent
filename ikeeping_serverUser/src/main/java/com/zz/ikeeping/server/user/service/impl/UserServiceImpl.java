f66248f515204dd5d74fcb252adbbefc2b28e3ba
qi <541694643@qq.com>
2019-07-02 20:53:42 +0800

    添加注释

--- ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/service/impl/UserServiceImpl.java
+++ ikeeping_serverUser/src/main/java/com/zz/ikeeping/server/user/service/impl/UserServiceImpl.java
@@ -7,15 +7,12 @@ import com.zz.ikeeping.common.config.ProjectConfig;
 import com.zz.ikeeping.common.vo.R;
 import com.zz.ikeeping.entity.User;
 
-import com.zz.ikeeping.message.core.service.MessageService;
 import com.zz.ikeeping.server.user.dao.UserMapper;
 import com.zz.ikeeping.server.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.security.util.Password;
 
-import javax.annotation.Resource;
 
 
 @Service
@@ -30,7 +27,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public R checkPhone(String phone) {
         User user=userMapper.selectByPhone(phone);
-        if(user!=null){
+        if(user == null){
             return R.setOK("当前手机号可用");
         }else {
             return R.setERROR("手机号已经注册过，请找回密码");
@@ -40,7 +37,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional(rollbackFor = {UserException.class})
-    public R save(User user) throws UserException {
+    public R insert(User user) throws UserException {
         if(userMapper.selectByPhone(user.getPhone())== null) {
             // 注册自动生成6位数字的密码
             int code = CodeUtil.createCode(6);
@@ -66,51 +63,51 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
-    public R insert(String phone, int code) {
+    public R verifyCode(String phone, int code) {
         if (userMapper.selectByPhone(phone) != null) {
             return R.setERROR("手机号已经注册过，请找回密码");
         }
         String key=ProjectConfig.SMSCODE+phone;
-            if(jedisUtil.exists(key)){
-                String v=jedisUtil.get(key);
-                if(v!=null){
-                    if(Integer.parseInt(v)==code){
-                        // 注册自动生成6位数字的密码
-                        int password = CodeUtil.createCode(6);
-                        System.out.println("mima"+password);
-                        String name = String.valueOf(CodeUtil.createCode(8));
+        if(jedisUtil.exists(key)){
+            String v=jedisUtil.get(key);
+            if(v!=null){
+                if(Integer.parseInt(v)==code){
+                    // 注册自动生成6位数字的密码
+                    int password = CodeUtil.createCode(6);
+                    System.out.println("mima"+password);
+                    String name = String.valueOf(CodeUtil.createCode(8));
                         /* System.out.println("jedis"+jedisUtil.get(ProjectConfig.SMSCODE + phone));
                         System.out.println("code"+code);*/
-                        System.out.println(Integer.parseInt(jedisUtil.get(ProjectConfig.SMSCODE + phone))==(code));
-                        try {
+                    System.out.println(Integer.parseInt(jedisUtil.get(ProjectConfig.SMSCODE + phone))==(code));
+                    try {
 
-                            if (jedisUtil.get(ProjectConfig.SMSCODE + phone).equals(String.valueOf(code))) {
-                                User user = new User();
-                                // 随机生成的密码进行加密
-                                user.setPassword(EncryptionUtil.RSAEnc(ProjectConfig.PASSRSAPRI, Integer.toString(password)));
-                                user.setUname(name);
-                                user.setPhone(phone);
-                                // 新增用户
-                                userMapper.insert(user);
-                                // 初始化其他
+                        if (jedisUtil.get(ProjectConfig.SMSCODE + phone).equals(String.valueOf(code))) {
+                            User user = new User();
+                            // 随机生成的密码进行加密
+                            user.setPassword(EncryptionUtil.RSAEnc(ProjectConfig.PASSRSAPRI, Integer.toString(password)));
+                            user.setUname(name);
+                            user.setPhone(phone);
+                            // 新增用户
+                            userMapper.insert(user);
+                            // 初始化其他
 
 
-                            }
-                        } catch (Exception e) {
-                            try {
-                                throw new UserException("注册异常" + e.getMessage());
-                            } catch (UserException e1) {
-                                e1.printStackTrace();
-                            }
                         }
-                        return R.setOK(name + "的初始密码为：", password);
-                    }else {
-                        return R.setERROR("验证码输入错误");
+                    } catch (Exception e) {
+                        try {
+                            throw new UserException("注册异常" + e.getMessage());
+                        } catch (UserException e1) {
+                            e1.printStackTrace();
+                        }
                     }
+                    return R.setOK(name + "的初始密码为：", password);
+                }else {
+                    return R.setERROR("验证码输入错误");
                 }
             }
+        }
 
-          return  R.setERROR("亲，验证码过期，请重新获取验证码");
+        return  R.setERROR("亲，验证码过期，请重新获取验证码");
 
     }
 
